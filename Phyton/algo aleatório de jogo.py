9e7fe3249767c12f00ae543607255d4d6ddb0b13
Andry :D <57609573+Nessa512@users.noreply.github.com>
2020-03-30 15:57:19 -0300

    Add files via upload

---
+++ Phyton/algo aleatório de jogo.py
@@ -0,0 +1,11 @@
+pygame.init()
+
+window = pygame.display.set_mode([640, 480])
+pygame.display.set_caption('Olá mundo!')
+
+window.fill([0, 0, 0])
+pygame.display.flip()
+
+input()
+
+pygame.quit()
---
+++ Phyton/teste pra frescar.py
@@ -0,0 +1,6 @@
+A = str(input("Quer saber como é pegar no fogo??\n"))
+if A == "sim":
+    print("Então pega, tu vai se queimar")
+elif  A == "nem":
+        print("Aí sim hein. Esse é sábio vet")
+
